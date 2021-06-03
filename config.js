5b533dc4b3ff1c598e8e1f137aab4cc9fd6cad05
tienhunga <85288996+tienhunga@users.noreply.github.com>
2021-06-03 18:14:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,4 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều nhiều 😘😘',
     messDesc: 'Sáng mai 5h anh qua đón nhé công túa nhợn.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+   }
